5fadf37bd0d8ae1c397ade1c076c158bfd6914e3
Huynh Minh Nghia <huynhminhnghia.hmn99@gamil.com>
2022-04-20 12:24:53 +0700

    topic 10

--- src/webdriver/Topic_10_Alert.java
+++ src/webdriver/Topic_10_Alert.java
@@ -2,6 +2,7 @@ package webdriver;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,8 +11,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_Template {
+public class Topic_10_Alert {
 	WebDriver driver;
+	Alert alert;
 	String projectPath = System.getProperty("user.dir");
 
 	@BeforeClass
@@ -25,7 +27,24 @@ public class Topic_01_Template {
 	}
 
 	@Test
-	public void TC_01() {
+	public void TC_01_Accept_Alert() {
+		driver.get("https://automationfc.github.io/basic-form/");
+
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		sleepInSecond(3);
+
+		// Switch qua Alert
+		alert = driver.switchTo().alert();
+
+		Assert.assertEquals(alert.getText(), "I am a JS Alert");
+		
+		alert.accept();
+		sleepInSecond(3);
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("p#result")).getText(), "You clicked an alert successfully");
+		
+		
+		
 		
 	}
 
