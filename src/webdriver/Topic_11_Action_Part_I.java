1396cfe5a2da70bf9850d37b25355a27c6b60f6b
HUYNH MINH NGHIA <huynhminhnghia.hmn99@gmail.com>
2022-04-23 06:54:41 +0700

    Topic 11 new

---
+++ src/webdriver/Topic_11_Action_Part_I.java
@@ -0,0 +1,56 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_11_Action_Part_I {
+	WebDriver driver;
+	Actions action;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+
+		action = new Actions(driver);
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+
+	@Test
+	public void TC_01() {
+		
+	}
+
+	@Test
+	public void TC_02() {
+		
+	}
+
+	@Test
+	public void TC_03() {
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	public void sleepInSecond(long second) {
+		try {
+			Thread.sleep(second * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
